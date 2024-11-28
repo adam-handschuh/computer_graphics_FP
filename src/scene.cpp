35e592840109c7dd8697f7543cabd0fce6ac0486
Adam <adam.o.handschuh@gmail.com>
2024-11-28 16:02:32 +0000

    Repository creation

--- src/scene.cpp
+++ src/scene.cpp
@@ -1,32 +1,72 @@
 #include "config.h"
 
 //VARIABLES
+static int windowWidth = 1366;
+static int windowHeight = 768;
 static GLFWwindow *window;   //Create window
 static void key_callback(GLFWwindow *window, int key, int scanCode, int action, int mode);
 
+//camera
+glm::vec3 camera_pos = glm::vec3(0.0f, 0.0f, 0.0f);
+glm::vec3 camera_up = glm::vec3(0.0f, 1.0f, 0.0f);
+glm::vec3 camera_lookAt(30, 30, 30);
+static float camera_fov = 45.0f;
+static float camera_near = 600.0f;
+static float camera_far = 2500.0f;
 
 //STRUCTS
+struct Scene{
+
+
+  void render(glm::mat4 vp){
+
+  }
+
+  void cleanup(){
+
+  }
+};
 
 //MAIN LOOP
 int main(){
   //SETTINGS
-  glfwInit();           //Initialise glfw
-  window = glfwCreateWindow(800,800, "My Window", NULL, NULL);
+  glfwInit();                                           //Initialise glfw
+  window = glfwCreateWindow(windowWidth,
+                            windowHeight,
+                            "My Window",
+                            NULL,
+                            NULL);
   glfwMakeContextCurrent(window);
-  gladLoadGLLoader((GLADloadproc)glfwGetProcAddress); //Initialise glad
+  gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);   //Initialise glad
 
   //PRE-RENDER SETTINGS
+  glEnable(GL_DEPTH_TEST);
+  glEnable(GL_CULL_FACE);
   glClearColor(0.5f,0.5f,0.5f,1.0f);
   glfwSetKeyCallback(window, key_callback);
 
+  Scene scene;
+
+  // Camera setup
+  glm::mat4 viewMatrix, projectionMatrix;
+  projectionMatrix = glm::perspective(glm::radians(camera_fov), (float)windowWidth / windowHeight, camera_near, camera_far);
 
   //RENDER LOOP
-  while(!glfwWindowShouldClose(window)){
-    glfwPollEvents();
+  do{
     glClear(GL_COLOR_BUFFER_BIT);
-    glfwSwapBuffers(window);
-  }
 
+    //Render Camera-View
+    viewMatrix = glm::lookAt(camera_pos, camera_lookAt, camera_up);
+    glm::mat4 vp =  projectionMatrix* viewMatrix;
+
+    scene.render(vp);
+
+    glfwSwapBuffers(window);
+    glfwPollEvents();
+  }while(!glfwWindowShouldClose(window));
+
+  //END PROCESSES
+  scene.cleanup();
   //Terminate glfw
   glfwTerminate();
   return 0;
